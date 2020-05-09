372b37f4b41f16c1fda60c0cda2c3edcd9830583
yoshitaka morita <s1180160@icloud.com>
2020-05-09 09:36:44 +0900

    Initial Commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by 森田祥考 on 2020/05/09.
+//  Copyright © 2020 森田祥考. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
